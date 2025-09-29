e027b16a5f36b5ed071e8b8cd7e2867885067719
Kaviya <kaviya3162@gmail.com>
2025-09-29 19:22:57 +0530

    Prepare for deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route,Navigate } from "react-router-dom";
 import Login from "./Login";
 import Welcome from "./Welcome";
 import Seekers from "./Seekers";
